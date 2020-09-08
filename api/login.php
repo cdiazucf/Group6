b3d770c9e66634964b2252a09e0ab1df1cc8ff8e
JonElliot Antognoni <jonantognoni@hotmail.com>
2020-09-08 18:56:06 -0400

    Debugged and tested login endpoint. Added functionality to return http response codes, error messsages, and updating the dateLastLoggedIn value in the database for the user upon login.

--- api/login.php
+++ api/login.php
@@ -9,40 +9,63 @@
     $loginData = json_decode($rawInput, true);
 
     // Create connection to database
-    $connection = new mysqli("localhost", "montyspy_jon", "phpSucks", "montyspy_cotrt");
+    $connection = new mysqli("localhost", "montyspy_camilo", "phpSucks", "montyspy_cotrt");
     
     if ($connection->connection_error)
     {
+        returnEmptyPayload("Failed to connect to database");
+        http_response_code(500);
         exit($connection->connection_error);
     }
     
-    $queryStatement = "SELECT ID, Firstname, Lastname FROM Users where Username='" . $loginData["username"] . "' and Password= '" . $loginData["password"] . "'";
+    $queryStatement = "SELECT ID, Firstname, Lastname FROM Users where Username='" . $loginData["username"] . "' and Password= '" . $loginData["password"] . "';";
     $queryData = $connection->query($queryStatement);
     
     if ($queryData->num_rows < 1)
     {
-        returnEmptyPayload();
+        returnEmptyPayload("User does not exist in database");
         $connection->close();
+        http_response_code(400);
         exit();
     }
     
     $data = $queryData->fetch_assoc();
-
-    // package and send the data to front-end
-    echo json_encode($data);
+    
+    // Update dateLastLoggedIn field in database
+    $updateResult = $queryStatement = "UPDATE Users SET DateLastLoggedIn='" . date_format(date(), "Y-m-d") . "' WHERE ID=" . $data["ID"] . ";";
+    
+    if ($updateResult == false)
+    {
+        returnJsonPayload($data["ID"], $data["Username"], $data["Firstname"], $data["Lastname"], $data["DateCreated"], date_format(date(), "Y-m-d"), "Failed to update DateLastLoggedIn in database");
+    }
+    else
+    {
+        returnJsonPayload($data["ID"], $data["Username"], $data["Firstname"], $data["Lastname"], $data["DateCreated"], date_format(date(), "Y-m-d"), "");
+    }
     
     $connection->close();
+    http_response_code(200);
     exit();
     
+    
+    /*
+    *   returnJsonPayload()
+    *
+    *   Returns JSON package to Front-end
+    */
+    function returnJsonPayload($id, $username, $firstname, $lastname, $dateCreated, $dateLastLoggedIn, $error)
+    {
+        $payload = array("ID" => $id, "Username" => $username, "Firstname" => $firstname, "Lastname" => $lastname, "DateCreated" => $dateCreated, "DateLastLoggedIn" => $dateLastLoggedIn, "Error" => $error);
+        echo json_encode($payload);
+    }
     
     /*
     *   returnEmptyPayload()
     *
     *   Return an empty JSON package to front-end when query returns no data
     */
-    function returnEmptyPayload()
+    function returnEmptyPayload($error)
     {
-        $payload = array("ID" => -1, "Username" => "", "Firstname" => "", "Lastname" => "", "DateCreated" => "", "DateLastLoggedIn" => "");
-        echo json_encode($payload);
+        returnJsonPayload(-1, "", "", "", "", "", $error);
     }
 ?>
